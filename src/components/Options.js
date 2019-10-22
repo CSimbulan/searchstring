b2e84ceaa2b6478e2b8fa9736062c09886f7ba9b
CSimbulan <c.simbulan@hotmail.com>
2019-10-22 03:18:16 -0400

    added option to change IV

--- src/components/Options.js
+++ src/components/Options.js
@@ -30,89 +30,48 @@ class Options extends Component {
   render() {
     return (
       <div className="Options">
-        <div className="Box">
-          <div className="OptionsHeader">Sort By</div>
-          <button
-            className={this.highlightSort("cp")}
-            onClick={() => this.props.changeSort("cp")}
-          >
-            CP
-          </button>
-          <button
-            className={this.highlightSort("iv")}
-            onClick={() => this.props.changeSort("iv")}
-          >
-            IV
-          </button>
-          <button
-            className={this.highlightSort("atk")}
-            onClick={() => this.props.changeSort("atk")}
-          >
-            ATK
-          </button>
-        </div>
-        <div className="Box">
-          <div className="OptionsHeader">Toggle</div>
-          <button
-            className={this.highlightToggle(this.props.options.toggle.nundo)}
-            onClick={this.props.toggleNundo}
-          >
-            Nundo (66.7%)
-          </button>
-          <button
-            className={this.highlightToggle(this.props.options.toggle.lvl15)}
-            onClick={this.props.toggleLvl15}
-          >
-            Level 15
-          </button>
-          <button
-            className={this.highlightToggle(this.props.options.toggle.under90)}
-            onClick={this.props.toggleUnder90}
-          >
-            Under 90%
-          </button>
-          <button
-            className={this.highlightToggle(this.props.options.toggle.color)}
-            onClick={this.props.toggleColor}
-          >
-            Colored Rows
-          </button>
-        </div>
-        <div className="Box">
-          <div className="OptionsHeader">CP Filter</div>
-          <button
-            className={this.highlightToggle(this.props.options.cpfilter)}
-            onClick={this.props.toggleCPFilter}
-          >
-            Show Only CP:
-          </button>
-          <br />
-          <input
-            value={this.props.filterCP}
-            onChange={this.props.onFilterChanged}
-            type="number"
-          />
-        </div>
-        <div className="Box">
-          <div className="OptionsHeader">Highest Level:</div>
-          <button
-            className={this.highlightLevel(40)}
-            onClick={() => this.props.changeHighestLevel(40)}
-          >
-            Level 40
-          </button>
-          <button
-            className={this.highlightLevel(35)}
-            onClick={() => this.props.changeHighestLevel(35)}
-          >
-            Level 35
-          </button>
-          <button
-            className={this.highlightLevel(30)}
-            onClick={() => this.props.changeHighestLevel(30)}
-          >
-            Level 30
-          </button>
+        <div className="Box-Wide">
+          <div className="Box-Wide-Div">
+            ATK:
+            <input
+              type="number"
+              min="0"
+              max="15"
+              value={this.props.options.atkiv}
+              onChange={this.props.onAtkChanged}
+            ></input>
+            <input className="invis" type="checkbox"></input>
+          </div>
+          <div className="Box-Wide-Div">
+            DEF:
+            <input
+              type="number"
+              min="0"
+              max="15"
+              value={this.props.options.defiv}
+              onChange={this.props.onDefChanged}
+            ></input>
+            <input className="invis" type="checkbox"></input>
+          </div>
+          <div className="Box-Wide-Div">
+            STA:
+            <input
+              type="number"
+              min="0"
+              max="15"
+              value={this.props.options.staiv}
+              onChange={this.props.onStaChanged}
+            ></input>
+            <input className="invis" type="checkbox"></input>
+          </div>
+          <div className="Box-Wide-Div">
+            Show Lvl 36-40{" "}
+            <input
+              className="CheckBox"
+              type="checkbox"
+              onChange={this.props.toggleShowAllLevels}
+            ></input>
+          </div>
         </div>
       </div>
     );
